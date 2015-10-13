4da1b54678fd314e7f8a983645f09f3e89b37781
cris2620 <cris26_skye@hotmail.com>
2015-10-12 23:08:10 -0500

    vw1.1

--- RPC_W/src/com/srpc/test/OtroTest.java
+++ RPC_W/src/com/srpc/test/OtroTest.java
@@ -1,16 +1,30 @@
 package com.srpc.test;
 
 import com.srpc.transport.RpcClient;
+import java.util.Scanner;
 
 public class OtroTest {
-
-	public static void main(String[] args) {
-		RpcClient client = new RpcClient("http://localhost:70/simpleRPC");		
-		Calculator cal = (Calculator)client.getProxy(Calculator.class, "CalculatorService");
-		int result = cal.sum2Num(8, 5);
-		System.out.println(result);
-		result = cal.sum2Num(-10, 5);
-		System.out.println(result);
-		client.stop();
-	}
+    public static void main(String[] args) {
+	RpcClient client = new RpcClient("http://localhost:70/simpleRPC");		
+	Calculator cal = (Calculator)client.getProxy(Calculator.class, "CalculatorService");
+	int value = 1;
+        int op1;
+        int op2;
+        Scanner scan = new Scanner(System.in);
+        while(value == 1){
+            System.out.println("Suma de Numeros Enteros ;D");
+            System.out.print("Ingrese el Primer Operando: ");
+            op1 = scan.nextInt();
+            System.out.print("Ingrese el Segundo Operando: ");
+            op2 = scan.nextInt();
+            value = cal.sum2Num(op1, op2);
+            System.out.println("El resultado es: "+value);
+            System.out.println("Desea COntinuar 1:SI 0:NO ");
+            do{
+                value = scan.nextInt();
+            }
+            while(!(value == 1 || value == 0));
+        }
+            client.stop();
+    }
 }
